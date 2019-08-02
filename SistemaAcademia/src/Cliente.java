99614f2cd3746b83b03ed9e62529bd084db81ae8
Roger <37402998+Roger987@users.noreply.github.com>
2019-08-01 21:56:35 -0300

    Projeto P4

---
+++ SistemaAcademia/src/Cliente.java
@@ -0,0 +1,10 @@
+
+public class Cliente {
+	
+	public String nome;
+	private String cpf;
+	public String email;
+	private String treino;
+	private boolean pagamento;
+	
+}
---
+++ SistemaAcademia/src/Main.java
@@ -0,0 +1,55 @@
+import java.util.*; 
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+		int input = -1;
+		
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+		ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+		ArrayList<Equipamento> equipamentos = new ArrayList<Equipamento>();
+	
+		System.out.println("\nBem-vindo!\nDigite a operação desejada:\n"
+				+ " 1 - Adicionar cliente\n 2 - Adicionar equipamento\n 3 - Adicionar funcionário\n 0 - Sair\n");
+		
+		while(input != 0) {
+			
+			try {
+				
+				input = scanner.nextInt();
+				scanner.nextLine();
+				
+				if(input == 0) {
+					System.out.println("\nPrograma encerrado\n");
+					break;
+				}
+				
+				switch(input) {
+				
+				case 1:
+					
+					Cliente cliente;
+					break;
+					
+				
+				}
+				
+				
+			}
+			
+			catch(Exception e) {
+				scanner.nextLine();
+				System.out.println("\nOps! Parece que aconteceu um erro!\n");
+				System.out.println("\nDigite a operação desejada:\n"
+					+ " 1 - Adicionar cliente\n 2 - Adicionar equipamento\n 3 - Adicionar funcionário\n 0 - Sair\n");
+
+			}
+			
+			
+		}
+		
+	}
+}
